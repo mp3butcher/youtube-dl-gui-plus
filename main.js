b5bc99039e79a4a7a292970b3b79988591b44f81
mp3butcher <julienvalentin51@gmail.com>
2025-02-07 15:56:08 +0100

    cleaning

--- main.js
+++ main.js
@@ -250,18 +250,6 @@ function createWindow(env) {
     });
 }
 
-function makeid(length) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-    return result;
-}
-
 app.on('ready', async () => {
     app.setAppUserModelId("com.mp3butcher.youtube-dl-gui");
     env = new Environment(app);
